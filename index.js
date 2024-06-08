e8384209b0d4a242c157c6b3ce10a889a43d1917
rahmaidle <rahmaidle254@gmail.com>
2024-06-08 12:40:59 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result;
+  if (distance <= 400){
+    result ="This one is on me!";
+  }
+  else if (distance > 2500){
+    result= 'No can do.';
+  }
+else {
+  result = "I will gladly take your thirty bucks.";
+
+}
+return result;
+  }
+
+
+function ternaryCheckCity(City){
+  // Write your code here!
+  return City ==='NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  let response;
+  switch (tip){
+    case 'generous':
+      response= 'Thank you so much.';
+      break;
+      case 'not as generous':
+        response='Thank you.';
+        break;
+        default:
+          response='Bye.';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
+  return response;
 }
